686055b471c14a6b7f074d546691b91c03af1fe2
JAYDA KADIR <jydkdr@gmail.com>
2019-07-09 12:06:19 +0100

    Add posts table to development database

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_100704) do
+ActiveRecord::Schema.define(version: 2019_07_09_110320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
+    t.string "type"
+    t.string "body"
+    t.string "address"
+    t.float "latitude"
+    t.float "logitude"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
