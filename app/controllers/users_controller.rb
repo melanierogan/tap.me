d0ffa33ef988ad7480690ba0f71f65eb507f834f
Ollie Porter <ol.porter@me.com>
2019-07-09 11:46:41 +0100

    Created route for new user with create action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome #{@user.username}"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password_digest)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
   root 'users#index'
+
+  get 'signup' => 'users#new'
+  resources :users
 end
