55a0bdc6a3da2a8fb839ba6c32b0a2200e7e6f02
Manisha Patel <ma9shap.gmail.com>
2019-07-11 11:36:29 +0100

    test setup

---
+++ test/system/end_point_test.rb
@@ -0,0 +1,17 @@
+require 'application_system_test_case'
+
+class EndPointTest < ApplicationSystemTestCase
+  # test 'end to end test' do
+  #   visit signup_url
+  #   fill_in 'user[username]', with: 'Jayda'
+  #   fill_in 'user[email]', with: 'jay@jay.com'
+  #   fill_in 'user[password]', with: '1234567890'
+  #   click_on('Sign Up')
+  #   visit new_post_url
+  #   fill_in 'post[choice]', with: 'Emotion'
+  #   fill_in 'post[body]', with: 'Content'
+  #   fill_in 'post[address]', with: '116 Goswell Road, London'
+  #   click_on('Save Post')
+  #   assert_text 'Content'
+  # end
+end
