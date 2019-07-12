fa7ea8fcc9691bd949a74fabe7532c11e29eb9e0
Manisha Patel <ma9shap.gmail.com>
2019-07-12 14:26:44 +0100

    Signup test passed

--- test/system/end_point_test.rb
+++ test/system/end_point_test.rb
@@ -1,6 +1,15 @@
 require 'application_system_test_case'
 
 class EndPointTest < ApplicationSystemTestCase
+  test 'signup' do
+    visit '/' 
+    click_on('Sign up here!')
+    fill_in 'user[username]', with: 'Jayda'
+    fill_in 'user[email]', with: 'jay@jay.com'
+    fill_in 'user[password]', with: '1234567890'
+    click_on('Sign Up')
+    assert_text 'Posts'
+  end
   # test 'end to end test' do
   #   visit signup_url
   #   fill_in 'user[username]', with: 'Jayda'
