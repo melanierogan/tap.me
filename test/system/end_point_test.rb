75495123a82c607059a84a316ff30ada85508670
JAYDA KADIR <jydkdr@gmail.com>
2019-07-12 14:44:20 +0100

    Test for logging in and out

--- test/system/end_point_test.rb
+++ test/system/end_point_test.rb
@@ -2,7 +2,7 @@ require 'application_system_test_case'
 
 class EndPointTest < ApplicationSystemTestCase
   test 'signup' do
-    visit '/' 
+    visit '/'
     click_on('Sign up here!')
     fill_in 'user[username]', with: 'Jayda'
     fill_in 'user[email]', with: 'jay@jay.com'
@@ -16,6 +16,16 @@ class EndPointTest < ApplicationSystemTestCase
     fill_in 'session[password]', with: '1234567'
     click_on('Log In')
     assert_text 'Posts'
+  end
+
+  test 'log in and out' do
+    visit '/'
+    fill_in 'session[email]', with: 'mel@mel.com'
+    fill_in 'session[password]', with: '1234567'
+    click_on('Log In')
+    assert_text 'Posts'
+    click_on('Log Out')
+    assert_text 'Sign up here!'
   end
     # test 'end to end test' do
   #   visit signup_url
