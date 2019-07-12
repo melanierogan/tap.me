a46b3c75478f1ad323f6f2f4e568840cf1325089
Manisha Patel <ma9shap.gmail.com>
2019-07-12 15:39:43 +0100

    test for posts delete created

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -30,4 +30,19 @@ class PostsTest < ApplicationSystemTestCase
     assert_text 'Test update'
   end
 
+  test 'delete post' do
+    visit '/'
+    fill_in 'session[email]', with: 'mel@mel.com'
+    fill_in 'session[password]', with: '1234567'
+    click_on('Log In')
+    click_on('+')
+    fill_in 'post[body]', with: 'Content'
+    fill_in 'post[address]', with: '116 Goswell Road, London'
+    click_on('Save Post')
+    assert_text 'Content'
+    click_link('Delete', :match => :first)
+    page.driver.browser.switch_to.alert.accept
+    refute_text 'Content'
+  end
+
 end
