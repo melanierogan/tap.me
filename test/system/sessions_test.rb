cd036b6a6481243b79df05c1dda2a41e3f401fdf
JAYDA KADIR <jydkdr@gmail.com>
2019-07-12 15:54:03 +0100

    Test for user deletion

--- test/system/sessions_test.rb
+++ test/system/sessions_test.rb
@@ -1,15 +1,7 @@
 require 'application_system_test_case'
 
 class SessionsTest < ApplicationSystemTestCase
-   test 'signup' do
-     visit '/'
-     click_on('Sign up here!')
-     fill_in 'user[username]', with: 'Jayda'
-     fill_in 'user[email]', with: 'jay@jay.com'
-     fill_in 'user[password]', with: '1234567890'
-     click_on('Sign Up')
-     assert_text 'Posts'
-   end
+
   test 'log_in' do
     visit '/'
     fill_in 'session[email]', with: 'mel@mel.com'
