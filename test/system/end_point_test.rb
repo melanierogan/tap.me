63e57545413a8a982af92c6536f5f31b07442930
JAYDA KADIR <jydkdr@gmail.com>
2019-07-12 15:21:54 +0100

    Test for adding a new post

--- test/system/end_point_test.rb
+++ test/system/end_point_test.rb
@@ -27,6 +27,18 @@ class EndPointTest < ApplicationSystemTestCase
     click_on('Log Out')
     assert_text 'Sign up here!'
   end
+
+  test 'new post' do
+    visit '/'
+    fill_in 'session[email]', with: 'mel@mel.com'
+    fill_in 'session[password]', with: '1234567'
+    click_on('Log In')
+    click_on('+')
+    fill_in 'post[body]', with: 'Content'
+    fill_in 'post[address]', with: '116 Goswell Road, London'
+    click_on('Save Post')
+    assert_text 'Content'
+  end
     # test 'end to end test' do
   #   visit signup_url
   #   fill_in 'user[username]', with: 'Jayda'
@@ -35,9 +47,7 @@ class EndPointTest < ApplicationSystemTestCase
   #   click_on('Sign Up')
   #   visit new_post_url
   #   fill_in 'post[choice]', with: 'Emotion'
-  #   fill_in 'post[body]', with: 'Content'
-  #   fill_in 'post[address]', with: '116 Goswell Road, London'
-  #   click_on('Save Post')
-  #   assert_text 'Content'
+
+
   # end
 end
