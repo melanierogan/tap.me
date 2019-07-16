13f278f54c66b532ddd75f28d5baea6e165e1ab5
JAYDA KADIR <jydkdr@gmail.com>
2019-07-16 12:14:14 +0100

    Added names in alphabeticall order - links still needed to gihub and/or linkd in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
-  def active_class(controller_name, action_name)
-   "active" if current_page?(controller: controller_name, action: action_name)
+  # def active_class(controller_name, action_name)
+  #  "active" if current_page?(controller: controller_name, action: action_name)
+  # end
+  def nav_link(link_text, link_path)
+  class_name = current_page?(link_path) ? 'current' : ''
+
+  content_tag(:li, :class => class_name) do
+    link_to link_text, link_path
   end
+end
 end
