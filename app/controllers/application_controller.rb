4489e2d655d436d7594e5e3be74123e42b2aa181
Ollie Porter <ol.porter@me.com>
2019-07-16 16:13:21 +0100

    Removed unnecessary helper method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?, :require_user, :active_class
+  helper_method :current_user, :logged_in?, :require_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -11,10 +11,6 @@ class ApplicationController < ActionController::Base
 
   def require_user
     redirect_to login_path unless logged_in?
-  end
-
-  def active_class(link_path)
-      current_page?(link_path) ? "active" : ""
   end
 
 end
