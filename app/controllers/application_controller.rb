1a0a8fd963ab06ceaecede3dbfe5cb96e5c93f4b
Ollie Porter <ol.porter@me.com>
2019-07-16 15:17:28 +0100

    Active link added to navbar based upon current page route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?, :require_user
+  helper_method :current_user, :logged_in?, :require_user, :active_class
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -13,6 +13,8 @@ class ApplicationController < ActionController::Base
     redirect_to login_path unless logged_in?
   end
 
-
+  def active_class(link_path)
+      current_page?(link_path) ? "active" : ""
+  end
 
 end
